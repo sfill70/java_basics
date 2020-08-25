90000d9333e2422173936624c5f2bcdbf9605476
sfill70 <sergfill70@yandex.ru>
2020-08-25 14:23:59 +0300

    init IntegerExperiments

---
+++ IntegerExperiments/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.count += 7843;
+        System.out.println(container.count);
+        System.out.println(sumDigits(container.count));
+        System.out.println(sumDigits(12345));
+        System.out.println(sumDigits(10));
+        System.out.println(sumDigits(5059191));
+
+        System.out.println(sumDigits2(container.count));
+        System.out.println(sumDigits2(12345));
+        System.out.println(sumDigits2(10));
+        System.out.println(sumDigits2(5059191));
+
+    }
+
+    public static int sumDigits(Integer number) {
+        int summ = 0;
+        String num = number.toString();
+        char[] chars = num.toCharArray();
+        for (char ch : chars
+        ) {
+            summ = summ + Character.getNumericValue(ch);
+        }
+        return summ;
+    }
+
+    public static int sumDigits2(int number) {
+        int num = number;
+        int summ = 0;
+        int tmp;
+        while (num >= 1) {
+            tmp = num % 10;
+            num /= 10;
+            summ += tmp;
+        }
+        return summ;
+    }
+
+}

f8629414836521203297100359ecffde893e56e6
sfill70 <sergfill70@yandex.ru>
2020-08-30 12:17:08 +0300

    lesson 4.2 work of bugs ExtremeValues

--- 04_NumbersStringsAndDates/ExtremeValues/src/Main.java
+++ 04_NumbersStringsAndDates/ExtremeValues/src/Main.java
@@ -17,29 +17,30 @@ public class Main {
         System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
         System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE);
 
+//        Поправил на  -Float.MAX_VALUE
         System.out.println("Запись в экспоненциальная форме числа");
-        System.out.println("Float.MIN_VALUE = " + Float.MIN_VALUE);
+        System.out.println("Минимальное (отрицательное) значение Float = " + (-Float.MAX_VALUE));
         System.out.println("Float.MAX_VALUE = " + Float.MAX_VALUE);
         System.out.println("Запись в десятичной форме числа");
+        System.out.println(String.format("%.0f", (-Float.MAX_VALUE)));
         System.out.println(String.format("%.0f", Float.MAX_VALUE));
+
 
 
         System.out.println("Long.MAX_VALUE = " + Long.MAX_VALUE);
         System.out.println("Long.MIN_VALUE = " + Long.MIN_VALUE);
+
+        //        Поправил на  -Double.MAX_VALUE
         System.out.println("Запись в экспоненциальная форме числа");
-        System.out.println("Double.MIN_VALUE = " + Double.MIN_VALUE);
+        System.out.println("Минимальное (отрицательное) значение Double = " + -Double.MAX_VALUE);
         System.out.println("Double.MAX_VALUE = " + Double.MAX_VALUE);
         System.out.println("Запись в десятичной форме числа");
         System.out.println(String.format("%.0f", Double.MAX_VALUE));
+        System.out.println(String.format("%.0f", -Double.MAX_VALUE));
 
         System.out.println(BigDecimal.valueOf(Double.MAX_VALUE));
         System.out.println(BigInteger.valueOf(Long.MAX_VALUE));
 
-//        Pattern pattern = Pattern.compile("<.*?>" , Pattern.CASE_INSENSITIVE);
-//        String s = "<hbtwrhb>tw4hbtw4 <HBT> 4hgw4h4w w4hw4h4wh <hbtwc>wechdt   svcwvcwe<1242532>";
-//        System.out.println(s);
-//        s = s.replaceAll(String.valueOf(pattern),"!!");
-//        System.out.println(s);
 
 
     }

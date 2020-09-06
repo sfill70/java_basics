8d188b2b1759dc8a9539a798e25c2bd550ac9271
sfill70 <sergfill70@yandex.ru>
2020-09-06 19:51:45 +0300

    edit Homework 5.2

--- 05_ArraysAndCollections/src/ToDoList2.java
+++ 05_ArraysAndCollections/src/ToDoList2.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
-public class ToDoList {
+public class ToDoList2 {
     static ArrayList<String> toDoList = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -27,8 +28,7 @@ public class ToDoList {
                 index = Integer.parseInt(information[1]);
                 toDoList.add(index - 1, data.substring(data.indexOf(information[1]) + information[1].length()));
             } catch (NumberFormatException e) {
-                toDoList.add(data.substring(data.indexOf(" ") + 1));
-                System.out.println("Добавлена запись - " + toDoList.size());
+                System.out.println("Неверный формат команды ADD - второй пареметр должен быть числом");
                 return;
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до " + (toDoList.size() + 1));
@@ -46,44 +46,27 @@ public class ToDoList {
     }
 
     static void editToDoList(String data) {
-        String[] information = data.split("\\s");
-        int index;
-        if (information.length >= 3) {
-            try {
-                index = Integer.parseInt(information[1]);
-                toDoList.set(index - 1, data.substring(data.indexOf(information[1]) + information[1].length()));
-            } catch (NumberFormatException e) {
-                System.out.println("Неверный формат команды EDIT - второй пареметр должен быть числом");
-                return;
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до" + toDoList.size());
-                return;
-            }
-        } else {
-            System.out.println("Неверный формат команды EDIT");
+        ParseData parseData = new ParseData(data,  "EDIT");
+        int index = parseData.getIndex();
+        String volume = parseData.getVolume();
+
+        try {
+            toDoList.set(index - 1, volume);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до " + toDoList.size());
             return;
         }
+
         System.out.println("Обновлена запись - " + (index));
 
     }
 
     static void deleteToDoList(String data) {
-        String[] information = data.split("\\s");
-        int index;
-        if (information.length == 2) {
-            try {
-                index = Integer.parseInt(information[1]);
-                toDoList.remove(index - 1);
-            } catch (NumberFormatException e) {
-                System.out.println("Неверный формат команды DELETE - второй пареметр должен быть числом");
-                return;
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Заданный номер выходит за границы списка,  допустимый номер от 1 до" + toDoList.size());
-                return;
-            }
-
-        } else {
-            System.out.println("Неверный формат команды DELETE");
+        int index = new ParseData(data, "DELETE").getIndex();
+        try {
+            toDoList.remove(index - 1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Заданный номер выходит за границы списка,  допустимый номер от 1 до" + toDoList.size());
             return;
         }
         System.out.println("Удалена запись " + (index));
@@ -133,4 +116,54 @@ public class ToDoList {
         scanner.close();
     }
 
+    static class ParseData {
+        String data;
+        int index;
+        String method;
+        String volume;
+
+        public ParseData(String data) {
+            this.data = data;
+            initIndexVolume(data);
+            method = "other";
+        }
+
+        public ParseData(String data, String method) {
+            this.data = data;
+            this.method = method;
+            initIndexVolume(data);
+        }
+
+        void initIndexVolume(String data) {
+            String[] information = Objects.requireNonNull(data).split("\\s");
+            if (information.length >= 3) {
+                try {
+                    this.index = Integer.parseInt(information[1]);
+                    this.volume = data.substring(data.indexOf(information[1]) + information[1].length());
+                } catch (NumberFormatException e) {
+                    System.out.println("Неверный формат команды" + method + "- второй пареметр должен быть числом");
+                }
+            } else if (information.length == 2) {
+                if (method.equals("ADD")) {
+                    this.volume = data.substring(data.indexOf(" ") + 1);
+                } else if (method.equals("DELETE")) {
+                    try {
+                        this.index = Integer.parseInt(information[1]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Неверный формат команды" + method + "- второй пареметр должен быть числом");
+                    }
+                }
+            } else {
+                System.out.println("Неверный формат команды" + method);
+            }
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public String getVolume() {
+            return volume;
+        }
+    }
 }

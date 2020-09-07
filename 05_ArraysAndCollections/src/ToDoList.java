e467254f701609991a353b2bc778d0529c6fe792
sfill70 <sergfill70@yandex.ru>
2020-09-07 15:23:14 +0300

    work on bugs Homework 5.2

--- 05_ArraysAndCollections/src/ToDoList.java
+++ 05_ArraysAndCollections/src/ToDoList.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ToDoList {
+    public static final String REG_CHECK_DATA = "(\\s+)(\\d+)(\\s+)(.+)";
+    public static final String REG_CHECK_DATA_ADD = "\\s+.+";
+    public static final String REG_CHECK_DATA_DEL = "\\s+\\d+";
+
+
     static ArrayList<String> toDoList = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -19,118 +24,89 @@ public class ToDoList {
         }
     }
 
-    static void addToDoList(String data) {
-        String[] information = data.split("\\s");
-        int index;
-        if (information.length >= 3) {
-            try {
-                index = Integer.parseInt(information[1]);
-                toDoList.add(index - 1, data.substring(data.indexOf(information[1]) + information[1].length()));
-            } catch (NumberFormatException e) {
-                toDoList.add(data.substring(data.indexOf(" ") + 1));
-                System.out.println("Добавлена запись - " + toDoList.size());
-                return;
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до " + (toDoList.size() + 1));
-                return;
-            }
-        } else if (information.length == 2) {
-            toDoList.add(data.substring(data.indexOf(" ") + 1));
+    static void addToDoList(String[] data) {
+        if (data.length == 2) {
+            toDoList.add(data[1]);
             System.out.println("Добавлена запись - " + toDoList.size());
-            return;
-        } else {
-            System.out.println("Неверный формат команды ADD");
-            return;
-        }
-        System.out.println("Добавлена запись - " + (index));
-    }
-
-    static void editToDoList(String data) {
-        String[] information = data.split("\\s");
-        int index;
-        if (information.length >= 3) {
-            try {
-                index = Integer.parseInt(information[1]);
-                toDoList.set(index - 1, data.substring(data.indexOf(information[1]) + information[1].length()));
-            } catch (NumberFormatException e) {
-                System.out.println("Неверный формат команды EDIT - второй пареметр должен быть числом");
-                return;
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до" + toDoList.size());
-                return;
+        } else if (data.length == 3) {
+            int index = Integer.parseInt(data[1]);
+            if (index > 0 && index - 1 <= toDoList.size()) {
+                toDoList.add(index - 1, data[2]);
+                System.out.println("Добавлена запись - " + (index));
+            } else {
+                System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до " + (toDoList.size() + 1));
             }
-        } else {
-            System.out.println("Неверный формат команды EDIT");
-            return;
         }
-        System.out.println("Обновлена запись - " + (index));
-
     }
 
-    static void deleteToDoList(String data) {
-        String[] information = data.split("\\s");
-        int index;
-        if (information.length == 2) {
-            try {
-                index = Integer.parseInt(information[1]);
-                toDoList.remove(index - 1);
-            } catch (NumberFormatException e) {
-                System.out.println("Неверный формат команды DELETE - второй пареметр должен быть числом");
-                return;
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Заданный номер выходит за границы списка,  допустимый номер от 1 до" + toDoList.size());
-                return;
-            }
-
+    static void editToDoList(String[] data) {
+        int index = Integer.parseInt(data[1]);
+        if (index > 0 && index <= toDoList.size()) {
+            toDoList.set(index - 1, data[2]);
+            System.out.println("Обновлена запись - " + (index));
         } else {
-            System.out.println("Неверный формат команды DELETE");
-            return;
+            System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до " + (toDoList.size()));
         }
-        System.out.println("Удалена запись " + (index));
     }
 
+    static void deleteToDoList(String[] data) {
+        int index = Integer.parseInt(data[1]);
+        if (index > 0 && index <= toDoList.size()) {
+            toDoList.remove(index - 1);
+            System.out.println("Удалена запись " + (index));
+        } else {
+            System.out.println("Заданный номер выходит за границы списка, допустимый номер от 1 до " + (toDoList.size()));
+        }
+    }
 
-    static void choice(String data) {
-        data = data.trim().replaceAll("\\s+", " ");
+    static String[] splitData(String data, int num) {
+        return data.trim().replaceAll("\\s+", " ").split("\\s", num);
+    }
+
+    static void processingCommands(String data) {
+        String[] dataProcessing = data.trim().replaceAll("\\s+", " ").split("\\s", 3);
         String command = data;
         if (data.contains(" ")) {
             command = data.substring(0, data.indexOf(" "));
         }
-        // Я не люблю switch
-        //не использовал startsWith что бы убрать зависимость от регистра
         if (command.equalsIgnoreCase("exit")) {
             return;
-        }
-        if (command.equalsIgnoreCase("LIST")) {
+        } else if (command.equalsIgnoreCase("list")) {
             viewToDoList();
-            System.out.println(data);
-            return;
+        } else if (command.equalsIgnoreCase("add")) {
+            if (data.trim().substring(data.indexOf(" ")).matches(REG_CHECK_DATA)) {
+                addToDoList(splitData(data, 3));
+            } else if (data.trim().substring(data.indexOf(" ")).matches(REG_CHECK_DATA_ADD)) {
+                addToDoList(splitData(data, 2));
+            } else {
+                System.out.println("Неверный формат команды ADD");
+            }
+        } else if (command.equalsIgnoreCase("edit")) {
+            if (data.trim().substring(data.indexOf(" ")).matches(REG_CHECK_DATA)) {
+                editToDoList(splitData(data, 3));
+            } else {
+                System.out.println("Неверный формат команды EDIT");
+            }
+        } else if (command.equalsIgnoreCase("delete")) {
+            if (data.trim().substring(data.indexOf(" ")).matches(REG_CHECK_DATA_DEL)) {
+                deleteToDoList(splitData(data, 2));
+            } else {
+                System.out.println("Неверный формат команды DELETE");
+            }
+        } else {
+            System.out.println("Неверная команда");
         }
-        if (command.equalsIgnoreCase("ADD")) {
-            addToDoList(data);
-            return;
-        }
-        if (command.equalsIgnoreCase("EDIT")) {
-            editToDoList(data);
-            return;
-        }
-        if (command.equalsIgnoreCase("DELETE")) {
-            deleteToDoList(data);
-            return;
-        }
-        System.out.println("Неверная команда");
     }
 
     static void runToDoList() {
         String command;
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("Введите команду " +
-                    "Для выхода введите - exit");
-            command = scanner.nextLine();
-            choice(command);
-        } while (!command.equalsIgnoreCase("exit"));
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            do {
+                System.out.println("Введите команду " +
+                        "Для выхода введите - exit");
+                command = scanner.nextLine();
+                processingCommands(command);
+            } while (!command.equalsIgnoreCase("exit"));
+        }
     }
-
 }

6c1b52303468bc0e0a63bd0d5f64adc8d60f316a
sfill70 <sergfill70@yandex.ru>
2020-09-18 22:57:42 +0300

    Homework 7.2 Lambda

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -5,39 +5,68 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Stream;
 
-public class Main
-{
-    private static String staffFile = "data/staff.txt";
+public class Main {
+    /*String root = "D:\\IdeaProjects\\java_basics\\07_AdvancedOOPFeatures";
+    String path = "D:\\IdeaProjects\\java_basics\\07_AdvancedOOPFeatures\\LambdaExpressions\\data";*/
+    static String getRoot = System.getProperty("user.dir");
+    static String dirSeparator = System.getProperty("file.separator");
+    private static String staffFile = getRoot + dirSeparator + "LambdaExpressions" + dirSeparator + "data/staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
         ArrayList<Employee> staff = loadStaffFromFile();
 
+        Comparator<Employee> comparator = (Employee o1, Employee o2) -> {
+            int compareSalary = (o1.getSalary().compareTo(o2.getSalary().compareTo(o2.getName().compareTo(o1.getName()))));
+            if ((compareSalary == 0)) {
+                return o1.getName().compareTo(o2.getName());
+            }
+            return compareSalary;
+        };
+        staff.sort(comparator);
 
+        //Покороче
+        staff.sort((o1, o2) -> {
+            int compareSalary;
+            return (compareSalary = o1.getSalary().compareTo(o2.getSalary())) == 0 ? o1.getName().compareTo(o2.getName()) : compareSalary;
+        });
+
+
+        for (Employee e : staff
+        ) {
+            System.out.println(e);
+
+        }
+
+        System.out.println("------------------/--------------------");
+
+        staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
+
+        for (Employee e : staff
+        ) {
+            System.out.println(e);
+
+        }
     }
 
-    private static ArrayList<Employee> loadStaffFromFile()
-    {
+    private static ArrayList<Employee> loadStaffFromFile() {
         ArrayList<Employee> staff = new ArrayList<>();
-        try
-        {
+        try {
             List<String> lines = Files.readAllLines(Paths.get(staffFile));
-            for(String line : lines)
-            {
+            for (String line : lines) {
                 String[] fragments = line.split("\t");
-                if(fragments.length != 3) {
+                if (fragments.length != 3) {
                     System.out.println("Wrong line: " + line);
                     continue;
                 }
                 staff.add(new Employee(
-                    fragments[0],
-                    Integer.parseInt(fragments[1]),
-                    (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
                 ));
             }
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return staff;

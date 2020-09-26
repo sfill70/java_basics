8e3d1737824abf6d657f2c369a8376ef9753a458
sfill70 <sergfill70@yandex.ru>
2020-09-26 13:07:06 +0300

    edit work on bugs Homework 8.1

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     private static final String REMOVE = "remove";
     private static final String ADD = "add";
     private static final String MESSAGE = "Неверный формат команды %s, введите help";
+    private static String additionalMessage;
     private static String addCommand = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static String commandExamples = "\t" + addCommand + "\n" +
@@ -21,28 +22,27 @@ public class Main {
         for (; ; ) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
-            if (tokens[0].equals(ADD)) {
-                try {
+            try {
+                if (tokens[0].equals(ADD)) {
+                    additionalMessage = ADD;
                     executor.addCustomer(tokens[1]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.printf(MESSAGE, ADD);
-                } catch (NameFormatException e) {
-                    System.out.print(e.getAdditionalMessage());
-                }
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals(REMOVE)) {
-                try {
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals(REMOVE)) {
+                    additionalMessage = REMOVE;
                     executor.removeCustomer(tokens[1]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.printf(MESSAGE, REMOVE);
+
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(commandError);
                 }
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(commandError);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.printf(MESSAGE, additionalMessage);
+            } catch (NameFormatException e) {
+                System.out.print(e.getAdditionalMessage());
             }
         }
     }

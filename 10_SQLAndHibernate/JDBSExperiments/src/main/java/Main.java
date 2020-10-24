5793f5eac4792f70104bf2c875a3920b2fb9209c
sfill70 <sergfill70@yandex.ru>
2020-10-24 23:47:32 +0300

    homework 10.1

---
+++ 10_SQLAndHibernate/JDBSExperiments/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String query = "SELECT course_name,COUNT(subscription_date) / COUNT(DISTINCT MONTH(subscription_date))" +
+                " AS AveragePurchase FROM PurchaseList GROUP BY course_name";
+        try (Connection connection = MysqlConnection.connectionMysql();
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString(1) + " - ");
+                System.out.println(resultSet.getString(2));
+            }
+        } catch (NullPointerException ex) {
+            System.out.println("Ошибка соеденения с базой данных");
+            ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ 10_SQLAndHibernate/JDBSExperiments/src/main/java/MysqlConnection.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+
+public class MysqlConnection {
+    static final String URL = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
+    //                +"useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static final String USER = "root";
+    static final String PASSWORD = "q1w2e3r4ty";
+
+    protected static Connection connectionMysql() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e
+        ) {
+            return null;
+        }
+    }
+}

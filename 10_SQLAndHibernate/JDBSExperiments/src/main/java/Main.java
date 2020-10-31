2a3d45e6d09ff054ae80b16567459ac450cd3d3d
sfill70 <sergfill70@yandex.ru>
2020-10-31 11:16:09 +0300

    Homework 10.4

--- 10_SQLAndHibernate/JDBSExperiments/src/main/java/Main.java
+++ 10_SQLAndHibernate/JDBSExperiments/src/main/java/Main.java
@@ -1,16 +1,20 @@
 import java.sql.*;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         String query = "SELECT course_name,COUNT(subscription_date) / (1 + max(MONTH(subscription_date))-min(MONTH(subscription_date)))" +
                 " AS AveragePurchase FROM PurchaseList GROUP BY course_name";
+        String query1 = "select st_id,id as id_course from (select student_name,course_name,id as st_id from purchaselist, " +
+                "students where purchaselist.student_name = students.name)as tb, courses where tb.course_name = courses.name order by st_id";
+
         try (Connection connection = MysqlConnection.connectionMysql();
              Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(query);
+            ResultSet resultSet = statement.executeQuery(query1);
             while (resultSet.next()) {
-                System.out.print(resultSet.getString(1) + " - ");
-                System.out.println(resultSet.getString(2));
+                System.out.print(resultSet.getInt(1) + " - ");
+                System.out.println(resultSet.getInt(2));
             }
         } catch (NullPointerException ex) {
             System.out.println("Ошибка соеденения с базой данных");
--- 10_SQLAndHibernate/JDBSExperiments/src/main/java/MysqlConnection.java
+++ 10_SQLAndHibernate/JDBSExperiments/src/main/java/MysqlConnection.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class MysqlConnection {
     static final String URL = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
-    //                +"useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+//                    +"useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     static final String USER = "root";
     static final String PASSWORD = "q1w2e3r4ty";
 

842ae9d3522311efb7559dbe2b76328416343380
sfill70 <sergfill70@yandex.ru>
2020-12-21 00:55:12 +0300

    Homework 14.1

---
+++ 14_Performance/NumberGeneratorMultithread/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    private static final int NUMBER_CORES = Runtime.getRuntime().availableProcessors();
+    private static final int REGION_NUMBER = 100;
+    private static final String RESULT_PATH = String.join(File.separator, System.getProperty("user.dir"), "res", File.separator);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println(RESULT_PATH);
+        long start = System.currentTimeMillis();
+        ExecutorService taskExecutor = Executors.newFixedThreadPool(NUMBER_CORES);
+
+        for (int i = 1; i <= REGION_NUMBER; i++) {
+            taskExecutor.execute(new RunNumberGenerator(RESULT_PATH, i));
+        }
+        taskExecutor.shutdown();
+        try {
+            taskExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println((System.currentTimeMillis() - start) + " ms");
+    }
+}
---
+++ 14_Performance/NumberGeneratorMultithread/src/main/java/RunNumberGenerator.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+public class RunNumberGenerator implements Runnable {
+    public static final int NUMBERS = 1000;
+    private String fileName;
+    private int regionCode;
+
+    public RunNumberGenerator(String fileName, int regionCode) {
+        this.fileName = fileName + regionCode + ".txt";
+        this.regionCode = regionCode;
+    }
+
+
+    @Override
+    public void run() {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+             PrintWriter printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(fileOutputStream)));) {
+            char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+            StringBuilder sb;
+            for (int number = 1; number < NUMBERS; number++) {
+                sb = new StringBuilder();
+                for (char firstLetter : letters) {
+                    for (char secondLetter : letters) {
+                        for (char thirdLetter : letters) {
+                            sb.append(firstLetter)
+                                    .append(padNumber(number, 3))
+                                    .append(secondLetter)
+                                    .append(thirdLetter)
+                                    .append(padNumber(regionCode, 2))
+                                    .append('\n');
+                        }
+                    }
+                }
+                printWriter.write(sb.toString());
+            }
+//            fileOutputStream.getChannel().force(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private StringBuilder padNumber(int number, int numberLength) {
+        StringBuilder numberStr = new StringBuilder();
+        int padSize = numberLength - Integer.toString(number).length();
+        numberStr.append("0".repeat(Math.max(0, padSize)));
+        return numberStr.append(number);
+    }
+}

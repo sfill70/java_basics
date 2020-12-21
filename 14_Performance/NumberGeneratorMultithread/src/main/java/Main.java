e02fab72d941b8fc8f4350d5912ca6f8a4af86f9
sfill70 <sergfill70@yandex.ru>
2020-12-21 08:40:04 +0300

    Homework v.2 14.1

--- 14_Performance/NumberGeneratorMultithread/src/main/java/Main.java
+++ 14_Performance/NumberGeneratorMultithread/src/main/java/Main.java
@@ -7,23 +7,30 @@ public class Main {
 
     private static final int NUMBER_CORES = Runtime.getRuntime().availableProcessors();
     private static final int REGION_NUMBER = 100;
-    private static final String RESULT_PATH = String.join(File.separator, System.getProperty("user.dir"), "res", File.separator);
+    private static final String RESULT_PATH = String.join(File.separator, System.getProperty("user.dir"), "res", "");
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
 
         System.out.println(RESULT_PATH);
-        long start = System.currentTimeMillis();
-        ExecutorService taskExecutor = Executors.newFixedThreadPool(NUMBER_CORES);
-
-        for (int i = 1; i <= REGION_NUMBER; i++) {
-            taskExecutor.execute(new RunNumberGenerator(RESULT_PATH, i));
-        }
-        taskExecutor.shutdown();
+        long start = 0;
         try {
-            taskExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        } catch (InterruptedException e) {
+            start = System.currentTimeMillis();
+
+            ExecutorService taskExecutor = Executors.newFixedThreadPool(NUMBER_CORES);
+
+            for (int i = 1; i <= REGION_NUMBER; i++) {
+                taskExecutor.execute(new RunNumberGenerator(RESULT_PATH, i));
+            }
+            taskExecutor.shutdown();
+            try {
+                taskExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 }
